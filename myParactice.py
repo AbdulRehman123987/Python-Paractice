5cf5b39cd3cc58ee8befbb7b41b7e9e0f654473e
AbdulRehman123987 <rehmanmirza458@gmail.com>
2024-11-20 12:48:54 +0500

    First 15 Days

---
+++ myParactice.py
@@ -0,0 +1,41 @@
+print("New file")
+# print("Hellow how are \"hehha\"\ you")
+# print("Hy",8,9,sep="~",end="--")
+# print("yes")
+# print(15/3)
+# print(15//3)
+# print(17%3)
+# print(2**70)
+
+#Calculator
+
+# value_one=int(input("Enter First value : "))
+# value_two=int(input("Enter Second value : "))
+
+# print(f"Sum of {value_one} and {value_two} is = ",value_one+value_two)
+# print(f"Multiplication of {value_one} and {value_two} is = ",value_one*value_two)
+# print(f"Division of {value_one} and {value_two} is = ",value_one/value_two)
+
+#Type Casting
+
+# c=1.1
+# d=9
+# print(c+d)
+
+# Exercise
+
+import time
+timestamp=time.strftime('%H:%M:%S')
+print('Current Time ::',timestamp)
+Hour=int(time.strftime('%H'))
+Minute=int(time.strftime('%M'))
+Second=int(time.strftime('%S'))
+
+if 5<=Hour<12:
+    print('Good Morning')
+elif 12 <= Hour < 17:
+    print('Good Afternoon')
+elif 17 <= Hour < 21:
+    print('Good Evening')
+else:
+    print('Good Night')
---
+++ paractice.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 16 00:11:41 2024
+
+@author: Abdul Rehman
+"""
+
+print("Hello")
+print("My first python program")
