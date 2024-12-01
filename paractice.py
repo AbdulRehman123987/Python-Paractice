8b1f2d0fe52f1f2c90622810243c58b204c3fcac
AbdulRehman123987 <rehmanmirza458@gmail.com>
2024-12-01 22:45:09 +0500

    File module

--- paractice.py
+++ paractice.py
@@ -5,5 +5,25 @@ Created on Sat Nov 16 00:11:41 2024
 @author: Abdul Rehman
 """
 
-print("Hello")
-print("My first python program")
+# def welcome():
+#     print("Welcome in paractice")
+
+# otherfile="Yes we are done it"
+# if __name__=="__main__":
+#     welcome()
+
+# print("Hello")
+# print("My first python program")
+
+#os module in python
+
+# import os
+
+# if(not os.path.exists("PythonData")):
+#     os.mkdir("PythonData")
+# for i in range (1,100):
+#     os.mkdir(f"PythonData/Day{i}")
+#list
+#folders=os.listdir("PythonData")
+# for i in range (1,100):
+#     os.rename(f"PythonData/Day{i}",f"Tutorial {i}")
